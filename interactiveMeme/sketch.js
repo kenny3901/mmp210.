ee8f0a982c62d0e36e4ea5fe1035f9d8ec92dff2
kenny smakovic <kenny3901@carmens-air.bmcc.cuny.edu>
2019-12-17 15:13:16 -0500

    Update sketch.js

--- interactiveMeme/sketch.js
+++ interactiveMeme/sketch.js
@@ -1,9 +1,20 @@
-function setup () {
-    createCanvas(600,600);
+var img;
+var img2;
+
+function preload() {
+	img = loadImage("2.jpg");
+    img2 =  loadImage("unknown.jpg");
+}
+
+function setup() {
+    createCanvas(796, 796);
 }
 
 function draw(){
-    background(300);
-    rectMode(CENTER);
+	image(img2, 0, 0);
+    textSize(50);
+    text("Deez Nuts",10,50);
+if (mouseIsPressed)
+    image (img,0,0);
     
-}
+	}	
